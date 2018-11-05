3d0547eb0479ae25f5241937f1b62e31c0326bf0
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-05 10:50:51 +0800

    Close subprocess in strtotime.py

--- strtotime.py
+++ strtotime.py
@@ -2,6 +2,8 @@ import os
 
 def strtotime(timestr):
 	path = os.path.dirname(os.path.realpath(__file__))
-	time = os.popen('php {}/strtotime.php "{}"'.format(path, timestr)).readline()
+	ps = os.popen('php {}/strtotime.php "{}"'.format(path, timestr))
+	time = ps.readline()
 	time = int(time)
+	ps.close()
 	return time
