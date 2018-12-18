636d5e50545ecfe3475a18914bbad89833ab0fc8
xiplus <huangxuanyuxiplus@gmail.com>
2018-12-18 14:35:48 +0800

    Monitor: Update reason for blackpage

--- Monitor.py
+++ Monitor.py
@@ -1141,8 +1141,8 @@ class Monitor():
                (`pagehash`, `wiki`, `page`, `timestamp`, `reason`, `point`)
                VALUES (%s, %s, %s, %s, %s, %s)
                ON DUPLICATE KEY
-               UPDATE `point` = `point` + %s, `timestamp` = %s""",
-            (pagehash, wiki, page, timestamp, reason, point, point, timestamp))
+               UPDATE `point` = `point` + %s, `timestamp` = %s, `reason` = %s""",
+            (pagehash, wiki, page, timestamp, reason, point, point, timestamp, reason))
         self.db.commit()
         message = "{}加入{}({})至監視頁面\n原因：{}".format(
                         msgprefix,
