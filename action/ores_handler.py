a3d3d1c257003a75e22a98614d0ff9ebfb8f1706
xiplus <huangxuanyuxiplus@gmail.com>
2019-06-19 23:18:31 +0800

    Add ores

---
+++ action/ores_handler.py
@@ -0,0 +1,62 @@
+import json
+import os
+import socket
+import sys
+import re
+import time
+import traceback
+import urllib.request
+
+sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../"))
+from Monitor import Monitor  # noqa: E402, pylint: disable=C0413
+from ores_config import config  # pylint: disable=E0611,W0614
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((config['host'], config['port']))
+sock.settimeout(10)
+
+M = Monitor()
+
+pool = []
+while True:
+    try:
+        starttime = time.time()
+        while True:
+            try:
+                data, address = sock.recvfrom(config['max_bytes'])
+                data = data.decode()
+                data = json.loads(data)
+                pool.append(data)
+                if len(pool) >= 10 or time.time() - starttime > 10:
+                    break
+            except socket.timeout:
+                break
+
+        if len(pool):
+            revids = '|'.join([str(data['revid']) for data in pool])
+            url = 'https://ores.wikimedia.org/v3/scores/zhwiki/?models=damaging&revids={}'.format(revids)
+            req = urllib.request.Request(url, headers={'User-Agent': M.wp_user_agent})
+            result = urllib.request.urlopen(req).read().decode("utf8")
+            result = json.loads(result)
+
+            for data in pool:
+                score = result['zhwiki']['scores'][str(data['revid'])]['damaging']['score']
+                if score['prediction']:
+                    summary = data['summary']
+                    summary = re.sub(r'/\*.*?\*/', '', summary)
+                    summary = re.sub(r'\s+', '', summary)
+                    message = '{}編輯{} {}（{}） 破壞可能性: {:.0f}%'.format(
+                        M.link_user(data['user']),
+                        M.link_page(data['page']),
+                        '無摘要' if summary == '' else '有摘要',
+                        M.link_diff(data['revid']),
+                        score['probability']['true'] * 100,
+                    )
+                    M.sendmessage(message, chat_id=config['chat_id'], token=config['token'])
+
+            pool = []
+
+    except Exception:
+        traceback.print_exc()
+        pool = []
---
+++ action/ores_sender.py
@@ -0,0 +1,45 @@
+import json
+import socket
+import traceback
+
+from ores_config import config  # pylint: disable=E0611,W0614
+
+
+def main(M, change):
+    try:
+        wiki = change['wiki']
+
+        if wiki != 'zhwiki':
+            return
+
+        if change['namespace'] != 0:
+            return
+
+        if change['bot']:
+            return
+
+        M.change_wiki_and_domain(change['wiki'], change['meta']['domain'])
+
+        M.addRC_wiki(change)
+
+        if 'revision' not in change:
+            print(change)
+            return
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect((config['host'], config['port']))
+        data = {
+            'revid': change['revision']['new'],
+            'summary': change['comment'],
+            'user': change['user'],
+            'page': change['title'],
+        }
+        print('send', data)
+        data = json.dumps(data)
+        data = data.encode()
+        sock.send(data)
+        sock.close()
+
+    except Exception:
+        traceback.print_exc()
+        M.error(traceback.format_exc())
