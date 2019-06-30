c2da9c2276967e9b55e7440d00018784dd9a8d99
xiplus <huangxuanyuxiplus@gmail.com>
2019-06-30 15:55:05 +0800

    Create message

---
+++ action/message.py
@@ -0,0 +1,152 @@
+import datetime
+import re
+import time
+import traceback
+
+from message_config import messagewiki, chats, token
+from message_function import block_flags, protect_description, parse_rights
+
+
+def main(M, change):
+    try:
+        day = '一二三四五六日'
+        timestamp = change['timestamp'] + 3600 * 8
+        timestr = time.strftime('%Y年%m月%d日 ({}) %H:%M', time.gmtime(timestamp)).format(
+            day[datetime.datetime.fromtimestamp(timestamp).weekday()])
+
+        wiki = change['wiki']
+
+        if wiki not in messagewiki:
+            return
+
+        # print(timestr, change)
+
+        M.change_wiki_and_domain(change['wiki'], change['meta']['domain'])
+
+        ctype = change['type']
+
+        message = ''
+
+        if ctype == 'log':
+            log_type = change['log_type']
+            log_action = change['log_action']
+
+            if log_type == 'block':
+                target = re.sub(r"^[^:]+:(.+)$", "\\1", change['title'])
+
+                if log_action == 'block':
+                    message = '#封禁 {0} {1}已封鎖{2}期限為{3}（{4}）（{5}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_user(target),
+                        change['log_params']['duration'],
+                        block_flags(change['log_params']['flags']),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+                elif log_action == 'reblock':
+                    message = '#封禁 {0} {1}已變更{2}的封鎖設定期限為{3}（{4}）（{5}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_user(target),
+                        change['log_params']['duration'],
+                        block_flags(change['log_params']['flags']),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+                elif log_action == 'unblock':
+                    message = '#封禁 {0} {1}已解除封鎖{2}（{3}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_user(target),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+            elif log_type == 'protect':
+                if log_action == 'protect':
+                    message = '#保護 {0} {1}已保護{2}{3}（{4}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_page(change['title']),
+                        protect_description(change['log_params']['description']),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+                elif log_action == 'modify':
+                    message = '#保護 {0} {1}已更改{2}的保護層級{3}（{4}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_page(change['title']),
+                        protect_description(change['log_params']['description']),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+                elif log_action == 'move_prot':
+                    pass
+
+                elif log_action == 'unprotect':
+                    message = '#保護 {0} {1}已移除{2}的保護（{3}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_page(change['title']),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+            elif log_type == 'newusers':
+                target = re.sub(r"^[^:]+:(.+)$", "\\1", change['title'])
+
+                message = '#新用戶 {0} {1}已建立使用者帳號{2}（{3}）'.format(
+                    timestr,
+                    M.link_user(change['user']) if change['user'] != target else '',
+                    M.link_user(target),
+                    M.parse_wikicode(change['comment']),
+                ).replace('（）', '')
+
+            elif log_type == 'rights':
+                target = re.sub(r"^[^:]+:(.+)$", "\\1", change['title'])
+
+                message = '#權限 {0} {1}已更改{2}的群組成員資格由{3}成為{4}（{5}）'.format(
+                    timestr,
+                    M.link_user(change['user']),
+                    M.link_user(target),
+                    parse_rights(change['log_params']['oldgroups'], change['log_params']['oldmetadata']),
+                    parse_rights(change['log_params']['newgroups'], change['log_params']['newmetadata']),
+                    M.parse_wikicode(change['comment']),
+                ).replace('（）', '')
+                print(message)
+
+            elif log_type == 'delete':
+                if log_action == 'delete':
+                    message = '#刪除 {0} {1}刪除頁面{2}（{3}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_page(change['title']),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+                elif log_action == 'delete_redir':
+                    # print(timestr, change)
+                    pass
+
+                elif log_action == 'restore':
+                    message = '#刪除 {0} {1}還原頁面{2}（{3}）'.format(
+                        timestr,
+                        M.link_user(change['user']),
+                        M.link_page(change['title']),
+                        M.parse_wikicode(change['comment']),
+                    ).replace('（）', '')
+
+                elif log_action == 'revision':
+                    # print(timestr, change)
+                    pass
+
+        if message:
+            print(message)
+            for chat_id in chats:
+                if chats[chat_id](change):
+                    print(chat_id)
+                    M.sendmessage(message, chat_id=chat_id, token=token)
+
+    except Exception:
+        traceback.print_exc()
+        M.error(traceback.format_exc())
---
+++ action/message_function.py
@@ -0,0 +1,63 @@
+def block_flags(flags):
+    f = {
+        'anononly': '僅限匿名使用者',
+        'noautoblock': '停用自動封鎖',
+        'nocreate': '停用帳號建立',
+        'noemail': '停用電子郵件',
+        'nousertalk': '無法編輯自己的對話頁面'
+    }
+    for key in f:
+        flags = flags.replace(key, f[key])
+    flags = flags.replace(',', '、')
+    return flags
+
+
+def protect_description(description):
+    r = {
+        '[edit=': '[編輯=',
+        '[move=': '[移動=',
+        '[create=': '[建立=',
+        '=autoconfirmed]': '=僅允許已自動確認的使用者]',
+        '=sysop]': '=僅限管理員]',
+    }
+    for key in r:
+        description = description.replace(key, r[key])
+    return description
+
+
+rightname = {
+    'accountcreator': '帳號建立員',
+    'autoreviewer': '巡查豁免者',
+    'bot': '機器人',
+    'bureaucrat': '行政員',
+    'confirmed': '已確認的使用者',
+    'eventparticipant': '活動參與者',
+    'filemover': '檔案移動員',
+    'flood': '機器使用者',
+    'interface-admin': '介面管理員',
+    'ipblock-exempt': 'IP 封鎖例外',
+    'massmessage-sender': '大量訊息傳送者',
+    'oversight': '監督員',
+    'patroller': '巡查員',
+    'rollbacker': '回退員',
+    'sysop': '管理員'
+}
+
+
+def parse_rights(group, metadata):
+    if len(group) == 0:
+        return '無'
+    res = []
+    for i in range(len(group)):
+        if 'expiry' in metadata[i]:
+            res.append('{}（{}/{}/{} {}:{}）'.format(
+                rightname[group[i]],
+                metadata[i]['expiry'][0:4],
+                metadata[i]['expiry'][4:6],
+                metadata[i]['expiry'][6:8],
+                metadata[i]['expiry'][8:10],
+                metadata[i]['expiry'][10:12],
+            ))
+        else:
+            res.append(rightname[group[i]])
+    return '、'.join(res)
