9a908978db6ad1c9ad73f3e45277f7135d0ede8a
xiplus <huangxuanyuxiplus@gmail.com>
2019-08-31 19:10:06 +0800

    Create retrieve_diff.py

---
+++ action/retrieve_diff.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import sys
+
+import diff_match_patch as dmp_module
+import pywikibot
+sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + '/../'))
+from Monitor import Monitor  # noqa: E402, pylint: disable=C0413
+
+os.environ['PYWIKIBOT2_DIR'] = os.path.dirname(os.path.realpath(__file__))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('revid', type=int)
+parser.add_argument('dir', type=str, nargs='?', default='temp')
+args = parser.parse_args()
+
+if not os.path.exists(args.dir):
+    os.makedirs(args.dir)
+
+M = Monitor()
+M.login()
+result = M.get_diff(args.revid, torelative='prev')
+
+removed_lines = '\n'.join(result['removed_lines'])
+added_lines = '\n'.join(result['added_lines'])
+
+dmp = dmp_module.diff_match_patch()
+diff = dmp.diff_main(removed_lines, added_lines)
+dmp.diff_cleanupSemantic(diff)
+
+text = ''
+for word in diff:
+    if word[0] == 1:
+        text += word[1]
+
+if text == '':
+    exit('Nothing to save\n')
+
+print(text)
+
+with open(os.path.join(args.dir, '{}.txt'.format(args.revid)), 'w', encoding='utf8') as f:
+    f.write(text)
