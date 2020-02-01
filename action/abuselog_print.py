a9a172601372d6f88806e91f35a1dc1f87b90161
xiplus <huangxuanyuxiplus@gmail.com>
2020-02-01 13:00:44 +0800

    Check details exists

--- action/abuselog_print.py
+++ action/abuselog_print.py
@@ -8,26 +8,27 @@ def main(M, log):
             return
 
         log_temp = log.copy()
-        KEY_TO_REMOVE = [
-            'added_lines',
-            'added_links',
-            'edit_diff',
-            'edit_diff_pst',
-            'new_html',
-            'new_pst',
-            'new_wikitext',
-            'old_wikitext',
-            'page_first_contributor',
-            'removed_lines',
-            'removed_links',
-            'tor_exit_node',
-            'user_groups',
-            'user_rights',
-        ]
-        for key in KEY_TO_REMOVE:
-            if key in log_temp['details']:
-                del log_temp['details'][key]
-        del log_temp['details']
+        if 'details' in log_temp:
+            KEY_TO_REMOVE = [
+                'added_lines',
+                'added_links',
+                'edit_diff',
+                'edit_diff_pst',
+                'new_html',
+                'new_pst',
+                'new_wikitext',
+                'old_wikitext',
+                'page_first_contributor',
+                'removed_lines',
+                'removed_links',
+                'tor_exit_node',
+                'user_groups',
+                'user_rights',
+            ]
+            for key in KEY_TO_REMOVE:
+                if key in log_temp['details']:
+                    del log_temp['details'][key]
+            del log_temp['details']
         print(log_temp)
 
     except Exception:
