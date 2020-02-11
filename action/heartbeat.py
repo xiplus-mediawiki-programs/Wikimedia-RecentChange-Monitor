d85e21f7d1a5145509eafa9cd1eaa1a3b22a5bc2
xiplus <huangxuanyuxiplus@gmail.com>
2020-02-11 10:11:37 +0800

    Update heartbeat.py

--- action/heartbeat.py
+++ action/heartbeat.py
@@ -11,6 +11,7 @@ M = Monitor()
 dbs = [
     ('RC_edit', 600),
     ('RC_log_abuselog', 3600),
+    ('rc_newusers_autocreate', 3600),
 ]
 
 timestamp = int(time.time())
@@ -19,6 +20,7 @@ for db in dbs:
     M.db_execute(f"""SELECT `timestamp` FROM {db[0]} ORDER BY `timestamp` DESC LIMIT 1""")
     row = M.db_fetchone()
     timediff = timestamp - row[0]
+    print(db[0], timediff)
     if timediff > db[1]:
         msg = '{}已有{}秒未收到訊息'.format(db[0], timediff)
         M.sendmessage(msg, chat_id=M.admin_chat_id, nolog=True)
