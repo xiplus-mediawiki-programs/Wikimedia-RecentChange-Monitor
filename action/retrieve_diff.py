f41a455ba5b08e934797f7623b01d7210f72414b
xiplus <huangxuanyuxiplus@gmail.com>
2020-11-06 16:02:35 +0800

    Retrieve summary

--- action/retrieve_diff.py
+++ action/retrieve_diff.py
@@ -18,6 +18,16 @@ if not os.path.exists(args.dir):
 
 M = Monitor()
 M.login()
+
+result = M.session.get(M.wp_api, params={
+    'action': 'query',
+    'format': 'json',
+    'prop': 'revisions',
+    'revids': args.revid,
+    'rvprop': 'comment'
+}).json()
+summary = list(result['query']['pages'].values())[0]['revisions'][0]['comment']
+
 result = M.get_diff(args.revid, torelative='prev')
 
 removed_lines = '\n'.join(result['removed_lines'])
@@ -27,7 +37,7 @@ dmp = dmp_module.diff_match_patch()
 diff = dmp.diff_main(removed_lines, added_lines)
 dmp.diff_cleanupSemantic(diff)
 
-text = ''
+text = summary + '\n'
 for word in diff:
     if word[0] == 1:
         text += word[1]
@@ -37,5 +47,5 @@ if text == '':
 
 print(text)
 
-with open(os.path.join(args.dir, '{}.txt'.format(args.revid)), 'w', encoding='utf8') as f:
+with open(os.path.join(args.dir, 'zhwiki-{}.txt'.format(args.revid)), 'w', encoding='utf8') as f:
     f.write(text)
