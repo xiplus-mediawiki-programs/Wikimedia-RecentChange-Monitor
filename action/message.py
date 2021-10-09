615813d9aca7df84969a8f43f43bdb5b8b03e139
xiplus <huangxuanyuxiplus@gmail.com>
2021-10-09 11:08:29 +0800

    Add zero width space

--- action/message.py
+++ action/message.py
@@ -43,62 +43,62 @@ def main(M, change):
                 target = re.sub(r"^[^:]+:(.+)$", "\\1", change['title'])
 
                 if log_action == 'block':
-                    message = '#封禁 {0} {1}已封鎖{2}期限為{3}（{4}）（{5}）'.format(
+                    message = '#封禁 {0} {1}已封鎖{2}期限為{3}（{4}）（\u200b{5}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_user(target),
                         change['log_params']['duration'],
                         block_flags(change['log_params']['flags']),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
                 elif log_action == 'reblock':
-                    message = '#封禁 {0} {1}已變更{2}的封鎖設定期限為{3}（{4}）（{5}）'.format(
+                    message = '#封禁 {0} {1}已變更{2}的封鎖設定期限為{3}（{4}）（\u200b{5}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_user(target),
                         change['log_params']['duration'],
                         block_flags(change['log_params']['flags']),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
                 elif log_action == 'unblock':
-                    message = '#封禁 {0} {1}已解除封鎖{2}（{3}）'.format(
+                    message = '#封禁 {0} {1}已解除封鎖{2}（\u200b{3}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_user(target),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
             elif log_type == 'protect':
                 if log_action == 'protect':
-                    message = '#保護 {0} {1}已保護{2}{3}（{4}）'.format(
+                    message = '#保護 {0} {1}已保護{2}{3}（\u200b{4}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_page(change['title']),
                         protect_description(change['log_params']['description']),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
                 elif log_action == 'modify':
-                    message = '#保護 {0} {1}已更改{2}的保護層級{3}（{4}）'.format(
+                    message = '#保護 {0} {1}已更改{2}的保護層級{3}（\u200b{4}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_page(change['title']),
                         protect_description(change['log_params']['description']),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
                 elif log_action == 'move_prot':
                     pass
 
                 elif log_action == 'unprotect':
-                    message = '#保護 {0} {1}已移除{2}的保護（{3}）'.format(
+                    message = '#保護 {0} {1}已移除{2}的保護（\u200b{3}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_page(change['title']),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
             elif log_type == 'newusers':
                 target = re.sub(r"^[^:]+:(.+)$", "\\1", change['title'])
@@ -110,46 +110,46 @@ def main(M, change):
                 if log_action == 'byemail':
                     email = '並且以電子郵件通知密碼'
 
-                message = '#新用戶 {0} {1}已{4}建立使用者帳號{2}{5}（{3}）'.format(
+                message = '#新用戶 {0} {1}已{4}建立使用者帳號{2}{5}（\u200b{3}\u200b）'.format(
                     timestr,
                     M.link_user(change['user']) if change['user'] != target else '',
                     M.link_user(target),
                     M.parse_wikicode(change['comment']),
                     auto,
                     email,
-                ).replace('（）', '')
+                ).replace('（\u200b\u200b）', '')
 
             elif log_type == 'rights':
                 target = re.sub(r"^[^:]+:(.+)$", "\\1", change['title'])
 
-                message = '#權限 {0} {1}已更改{2}的群組成員資格由{3}成為{4}（{5}）'.format(
+                message = '#權限 {0} {1}已更改{2}的群組成員資格由{3}成為{4}（\u200b{5}\u200b）'.format(
                     timestr,
                     M.link_user(change['user']),
                     M.link_user(target),
                     parse_rights(change['log_params']['oldgroups'], change['log_params']['oldmetadata']),
                     parse_rights(change['log_params']['newgroups'], change['log_params']['newmetadata']),
                     M.parse_wikicode(change['comment']),
-                ).replace('（）', '')
+                ).replace('（\u200b\u200b）', '')
 
             elif log_type == 'delete':
                 if log_action == 'delete':
-                    message = '#刪除 {0} {1}刪除頁面{2}（{3}）'.format(
+                    message = '#刪除 {0} {1}刪除頁面{2}（\u200b{3}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_page(change['title']),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
                 elif log_action == 'delete_redir':
                     pass
 
                 elif log_action == 'restore':
-                    message = '#刪除 {0} {1}還原頁面{2}（{3}）'.format(
+                    message = '#刪除 {0} {1}還原頁面{2}（\u200b{3}\u200b）'.format(
                         timestr,
                         M.link_user(change['user']),
                         M.link_page(change['title']),
                         M.parse_wikicode(change['comment']),
-                    ).replace('（）', '')
+                    ).replace('（\u200b\u200b）', '')
 
                 elif log_action == 'revision':
                     pass
